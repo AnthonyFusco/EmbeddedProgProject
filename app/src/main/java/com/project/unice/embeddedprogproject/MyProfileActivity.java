ef31b1165ad35a38bf45374aeb96835de064ad69
earendil06 <flopastor06@gmail.com>
2017-04-10 20:55:49 +0200

    my profile

---
+++ app/src/main/java/com/project/unice/embeddedprogproject/MyProfileActivity.java
@@ -0,0 +1,13 @@
+package com.project.unice.embeddedprogproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_profile);
+    }
+}
