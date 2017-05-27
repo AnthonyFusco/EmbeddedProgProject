36a3f5f8e54e4189bfa5940c19a0d5a615958a05
earendil06 <flopastor06@gmail.com>
2017-05-27 23:15:11 +0200

    view activity

--- app/src/main/java/com/project/unice/embeddedprogproject/ViewBusinessCardActivity.java
+++ app/src/main/java/com/project/unice/embeddedprogproject/ViewBusinessCardActivity.java
@@ -6,18 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.project.unice.embeddedprogproject.databaseModels.BusinessCard;
 import com.project.unice.embeddedprogproject.databaseModels.Contact;
 import com.project.unice.embeddedprogproject.sqlite.DataBaseManager;
 import com.project.unice.embeddedprogproject.sqlite.DataBaseTableManager;
 import com.project.unice.embeddedprogproject.sqlite.IDatabaseManager;
 
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Pretty display of a Business Card.
  */
 public class ViewBusinessCardActivity extends AppCompatActivity {
     public static final String CONTACT_INTENT_CODE = "contact";
-    private BusinessCard card;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,28 +34,53 @@ public class ViewBusinessCardActivity extends AppCompatActivity {
             Gson gson = new Gson();
             Contact contact = gson.fromJson(contactSerialized, Contact.class);
             IDatabaseManager manager = new DataBaseTableManager(getApplicationContext(), DataBaseManager.DATABASE_NAME);
-            card = (BusinessCard) manager.findFirstValue(BusinessCard.class, "id", contact.idBusinessCard);
-            fillCard(contact);
+            BusinessCard card = (BusinessCard) manager.findFirstValue(BusinessCard.class, "Phone", contact.phone);
+            fillCard(card);
         }
     }
 
     /**
      * Fill the view with the data of the given contact.
-     * @param contact the contact to add to the view
+     * @param card the BC to add to the view
      */
-    private void fillCard(Contact contact) {
+    private void fillCard(BusinessCard card) {
+        Type type = new TypeToken<Map<String, List<String>>>(){}.getType();
+        Map<String, List<String>> map = new Gson().fromJson(card.contains, type);
+
+        String phoneId = "Phone";
+        String emailId = "Email";
+        String organizationId = "Organization";
+        String websiteId = "Website";
+        String structuredNameId = "StructuredName";
+        String structuredPostalId = "StructuredPostal";
+
+
         TextView society = (TextView) findViewById(R.id.textViewSociety);
-        TextView fistName = (TextView) findViewById(R.id.textViewFirstName);
         TextView name = (TextView) findViewById(R.id.textViewName);
-        TextView adress = (TextView) findViewById(R.id.textViewAddressSociety);
+        TextView address = (TextView) findViewById(R.id.textViewAddressSociety);
         TextView email = (TextView) findViewById(R.id.textViewEmail);
         TextView phone = (TextView) findViewById(R.id.textViewPhone);
 
-        society.setText("");
-        fistName.setText("");
-        name.setText(contact.name);
-        adress.setText("");
-        email.setText("");
-        phone.setText(contact.phone);
+
+        String org = formatedStringValues(map.get(organizationId));
+        String na = formatedStringValues(map.get(structuredNameId));
+        String post =formatedStringValues(map.get(structuredPostalId));
+        String mail = formatedStringValues(map.get(emailId));
+        String pho = formatedStringValues(map.get(phoneId));
+
+        society.setText(org);
+        name.setText(na);
+        address.setText(post);
+        email.setText(mail);
+        phone.setText(pho);
+    }
+
+    private String formatedStringValues(List<String> list){
+        if (list == null) return "";
+        StringBuilder builder = new StringBuilder();
+        for (String s : list) {
+            builder.append(s).append('\n');
+        }
+        return builder.toString();
     }
 }
